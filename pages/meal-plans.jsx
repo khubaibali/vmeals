20b65e6e2da73a90df6c0980867fa68b2d358c64
nabeelkhancs <34935048+nabeelkhancs@users.noreply.github.com>
2022-12-10 23:58:19 +0500

    meal plan

--- pages/meal-plans.jsx
+++ pages/meal-plans.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import  Ourplanspage from './src/components/Our Plans/Index'
+import {getServerSideProps as headerProps} from './src/components/Common/Navbar'
+import {getServerSideProps as sliderBarProps} from './src/components/Home/Hero'
+import {getServerSideProps as builtDataProps} from './src/components/Home/Built'
+import {getServerSideProps as ourGeniusDataProps} from './src/components/Home/Genius'
+import {getServerSideProps as homeFitnessDataProps} from './src/components/Home/Fitness'
+import {getServerSideProps as ourHomeBlogsDataProps} from './src/components/Home/Ourblog'
+import {getServerSideProps as socialMediaIconsProps} from './src/components/Common/Footer'
 
-
-export default function ourplans() {
+export default function ourplans(props) {
   return (
     <div>
-        <Ourplanspage/>
+        <Ourplanspage headerData={props?.headerData} sliderBarData={props.sliderBarData} builtData={props.builtData} ourGeniusData={props.ourGeniusData} homeFitnessData={props.homeFitnessData} ourHomeBlogData ={props.ourHomeBlogData} socialMediaIcon={props.socialMediaIcon} footerData={props.footerData} tradeMarkData={props.tradmark}  />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  try {
+    console.log("calling")
+    let data = await headerProps()
+    let sliderBarData = await sliderBarProps()
+    let builtData = await builtDataProps()
+    let ourGeniusData = await ourGeniusDataProps()
+    let homeFitnessData = await homeFitnessDataProps()
+    let ourHomeBlogData = await ourHomeBlogsDataProps()
+    let socialMediaIcon = await socialMediaIconsProps()
+    console.log("header props",sliderBarData)
+     return {
+       props: { 
+         ...data.props,
+         ...sliderBarData.props,
+         ...builtData.props,
+         ...ourGeniusData.props,
+         ...homeFitnessData.props,
+         ...ourHomeBlogData.props,
+         ...socialMediaIcon.props
+       }, // will be pa
+      }
+  } catch (error) {
+    return{
+      props:{headerData:{},sliderBarData:{},builtData:[],ourGeniusData:[],homeFitnessData:[],ourHomeBlogData:[],socialMediaIcon:[]
+    }
+    }
+  }
+
 }
--- pages/src/components/Our Plans/Index.js
+++ pages/src/components/Our Plans/Index.js
@@ -8,21 +8,21 @@ import Fitness from "../Home/Fitness";
 import Built from "../Home/Built";
 import Question from "../Faq/Questions";
 
-export default function Index() {
+export default function Index({headerData,sliderBarData,builtData,ourGeniusData,homeFitnessData,ourHomeBlogData,socialMediaIcon,footerData,tradeMarkData}) {
   return (
     <>
       <div className="ourplansbg">
-        <Navbar />
-        <Hero />
+        <Navbar headerData={headerData} />
+        <Hero sliderBarData={sliderBarData} />
       </div>
       <Meals />
-      <Fitness />
-      <Built />
+      <Fitness homeFitnessData={homeFitnessData} />
+      <Built builtData={builtData} />
       <div className="bg-green-light  pt-[235px] -mt-[241px] sm:pt-[131px] sm:-mt-[98px] lg:pt-[290px] lg:-mt-[160px] ">
-        <Question />
+        {/* <Question /> */}
       </div>
       <Review />
-      <Fotter />
+      <Fotter socialMediaIcon={socialMediaIcon} footerData={footerData} tradeMarkData={tradeMarkData} />
     </>
   );
 }
