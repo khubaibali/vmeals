72cc87ca288545f574111d44885a5f0d3e7cf27e
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-10 14:09:09 +0500

    index for homescreen

--- pages/src/components/Home/Index.js
+++ pages/src/components/Home/Index.js
@@ -7,21 +7,21 @@ import Fitness from "./Fitness";
 import Ourblog from "./Ourblog";
 import Fotter from "../Common/Footer";
 import Review from "../About Us/Review";
-export default function Index({headerData}) {
+export default function Index({headerData,sliderBarData,builtData,ourGeniusData,homeFitnessData,ourHomeBlogData,socialMediaIcon,footerData,tradeMarkData}) {
   return (
     <>
       <div className="hero-bg  relative">
         <Navbar headerData={headerData}/>
-        <Hero />
+        <Hero sliderBarData={sliderBarData}/>
       </div>
       <div className="mt-[309px] md:mt-20 lg:mt-0 sm:mt-40">
-        <Built />
+        <Built builtData={builtData}/>
       </div>
-      <Genius />
-      <Fitness />
+      <Genius ourGeniusData={ourGeniusData}/>
+      <Fitness homeFitnessData={homeFitnessData}/>
       <Review />
-      <Ourblog />
-      <Fotter />
+      <Ourblog ourHomeBlogData={ourHomeBlogData}/>
+      <Fotter socialMediaIcon={socialMediaIcon} footerData={footerData} tradeMarkData={tradeMarkData} />
     </>
   );
 }
