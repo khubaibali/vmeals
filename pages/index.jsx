0b0a8c1d9e8397a30561feb5a388a7c3a6e1860d
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-10 14:10:38 +0500

    pages main index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Homepage from './src/components/Home/Index'
 import React, { useState } from 'react'
-import {Navbar,getServerSideProps as headerProps} from './src/components/Common/Navbar'
-
-function Home({props}) {
+import {getServerSideProps as headerProps} from './src/components/Common/Navbar'
+import {getServerSideProps as sliderBarProps} from './src/components/Home/Hero'
+import {getServerSideProps as builtDataProps} from './src/components/Home/Built'
+import {getServerSideProps as ourGeniusDataProps} from './src/components/Home/Genius'
+import {getServerSideProps as homeFitnessDataProps} from './src/components/Home/Fitness'
+import {getServerSideProps as ourHomeBlogsDataProps} from './src/components/Home/Ourblog'
+import {getServerSideProps as socialMediaIconsProps} from './src/components/Common/Footer'
+function Home(props) {
   console.log("home",props)
   return (
 
     <>
-      <Homepage headerData={props.headerData}/>
+      <Homepage headerData={props?.headerData} sliderBarData={props.sliderBarData} builtData={props.builtData} ourGeniusData={props.ourGeniusData} homeFitnessData={props.homeFitnessData} ourHomeBlogData ={props.ourHomeBlogData} socialMediaIcon={props.socialMediaIcon} footerData={props.footerData} tradeMarkData={props.tradmark} />
     </>
   )
 }
@@ -18,15 +23,28 @@ export async function getServerSideProps() {
   try {
     console.log("calling")
    let data = await headerProps()
-   console.log("header props",data)
+   let sliderBarData = await sliderBarProps()
+   let builtData = await builtDataProps()
+   let ourGeniusData = await ourGeniusDataProps()
+   let homeFitnessData = await homeFitnessDataProps()
+   let ourHomeBlogData = await ourHomeBlogsDataProps()
+   let socialMediaIcon = await socialMediaIconsProps()
+   console.log("header props",sliderBarData)
     return {
       props: { 
-        ...data
+        ...data.props,
+        ...sliderBarData.props,
+        ...builtData.props,
+        ...ourGeniusData.props,
+        ...homeFitnessData.props,
+        ...ourHomeBlogData.props,
+        ...socialMediaIcon.props
       }, // will be passed to the page component as props
     }
   } catch (error) {
     return{
-      props:{headerData:[]}
+      props:{headerData:{},sliderBarData:{},builtData:[],ourGeniusData:[],homeFitnessData:[],ourHomeBlogData:[],socialMediaIcon:[]
+    }
     }
   }
 
