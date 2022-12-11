9ffb7deba532651d335ed7fbb79426622b0de535
nabeelkhancs <34935048+nabeelkhancs@users.noreply.github.com>
2022-12-11 11:13:08 +0500

    green diet

--- pages/green-diet.jsx
+++ pages/green-diet.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
-import  GreenDietpage from './src/components/Green Diet/Index'
+import GreenDietpage from './src/components/Green Diet/Index'
+import { getServerSideProps as headerProps } from './src/components/Common/Navbar'
+import { getServerSideProps as builtDataProps } from './src/components/Home/Built'
+import { getServerSideProps as contentDataProps } from './src/components/Classic Diet/Customizeplan'
+import { getServerSideProps as socialMediaIconsProps } from './src/components/Common/Footer'
+import { getServerSideProps as contentDataPropsPage } from './src/components/Green Diet/Index'
 
-export default function greendiet() {
+export default function greendiet(props) {
   return (
     <div>
-        <GreenDietpage/>
+      <GreenDietpage
+        metaData={props?.contentData}
+        contentData={props?.contentDataGreenDiet}
+        headerData={props?.headerData}
+        builtData={props.builtData}
+        socialMediaIcon={props.socialMediaIcon}
+        footerData={props.footerData}
+        tradeMarkData={props.tradmark}
+      />
     </div>
   )
+}
+
+export async function getServerSideProps(constext) {
+  try {
+    console.log("calling class")
+    let data = await headerProps()
+    let builtData = await builtDataProps()
+    let contentData = await contentDataProps();
+    let socialMediaIcon = await socialMediaIconsProps()
+    let contentDataPage = await contentDataPropsPage()
+    console.log("header props", contentDataPage)
+    return {
+      props: {
+        ...data.props,
+        ...builtData.props,
+        ...contentData.props,
+        ...socialMediaIcon.props,
+        ...contentDataPage.props
+      }, // will be passed to the page component as props
+    }
+  } catch (error) {
+    return {
+      props: {
+        headerData: {}, sliderBarData: {}, builtData: [], ourGeniusData: [], homeFitnessData: [], ourHomeBlogData: [], socialMediaIcon: []
+      }
+    }
+  }
+
 }
--- pages/src/components/Green Diet/Index.js
+++ pages/src/components/Green Diet/Index.js
@@ -7,22 +7,46 @@ import Built from "../Home/Built";
 import Question from "../Faq/Questions";
 import Customizeplan from "../Classic Diet/Customizeplan";
 import Simplemenu from "../Classic Diet/Simplemenu";
+import SEO from "../Common/SEO";
+import { vmealsGreenDietContent } from "../../../../src/lib/APICommunications";
 
-export default function Index() {
+export default function Index({ headerData, builtData, socialMediaIcon, footerData, tradeMarkData, contentData, metaData }) {
+  const metaDataContent = Object.values(metaData).find(c => c.title == "Green Diet")
+  const contentDataGreenDiet = Object.values(contentData).find(c => c.VmealsGreenDietEnableDisables == "Enable")
   return (
     <>
+      <SEO pageTitle={metaDataContent?.meta?.title} metaText={metaDataContent?.meta?.description} />
+
       <div className="greendietbg">
-        <Navbar />
+        <Navbar  headerData={headerData}  />
         <Hero />
       </div>
-      <Customizeplan />
-      <Simplemenu />
-      <Built />
+      <Customizeplan  heading={contentDataGreenDiet?.VmealsGreenDietHeading} description={contentDataGreenDiet?.VmealsGreenDietdescriptionParagraphs} selectedPlan={"GreenDietVegan"}  />
+      <Simplemenu/>
+      <Built  builtData={builtData} />
       <div className="bg-green-light  pt-[235px] -mt-[241px] sm:pt-[131px] sm:-mt-[98px] lg:pt-[290px] lg:-mt-[160px] ">
-        <Question />
+        {/* <Question /> */}
       </div>
       <Review />
-      <Fotter />
+      <Fotter socialMediaIcon={socialMediaIcon} footerData={footerData} tradeMarkData={tradeMarkData} />
     </>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+   
+    let contentDataGreenDiet = await fetch(vmealsGreenDietContent)
+    let data = await contentDataGreenDiet.json()
+    console.log("slider bar ->>",data)
+   
+    return {
+      props: { contentDataGreenDiet: { ...data?.docs } }, // will be passed to the page component as props
+    }
+  } catch (error) {
+    return {
+      props: { contentDataGreenDiet: [] }
+    }
+  }
+
 }
