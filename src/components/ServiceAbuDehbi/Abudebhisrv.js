b61f03802f66ff8e80290e2597f6e82f4b61a11a
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-12 03:31:05 +0500

    spelling change of seo

--- src/components/ServiceAbuDehbi/Abudebhisrv.js
+++ src/components/ServiceAbuDehbi/Abudebhisrv.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Seo from '../Common/Seo'
+import SEO from '../Common/Seo'
 export default function Abudebhisrv() {
   return (
     <div>
            <div className=" w-11/12 2xl:max-w-[1600px] ml-auto mr-auto my-10 sm:my-20">
            <h1 class=" text-base sm:text-2xl 2xl:text-4xl f-f-li captalize text-green tracking-[1px] lg:tracking-[0.22em] text-center xl:text-left">Meal Plan Abu Dhabi</h1>
-           <Seo/>
+           <SEO/>
            </div>
 
     </div>
