26f656a8dadd2eca6d4362a79c01ddf4d2c7f6de
Sanaver <54433229+Sanaver@users.noreply.github.com>
2022-12-12 23:04:57 +0500

    new pages

---
+++ pages/termcondition.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  Termcondition from '../src/components/Term Condition/Index'
+
+export default function termcondition() {
+  return (
+    <div>
+        <Termcondition/>
+    </div>
+  )
+}
