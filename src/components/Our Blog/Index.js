dc08e21651f0b5ca85e77773651862b962fc78da
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-14 02:35:29 +0500

    seo added to our blog

--- src/components/Our Blog/Index.js
+++ src/components/Our Blog/Index.js
@@ -4,15 +4,15 @@ import Hero from "./Hero";
 import Fotter from "../Common/Footer";
 import Category from "./Category";
 
-export default function Index() {
+export default function Index({headerData,socialMediaIcon,tradeMarkData,footerData}) {
   return (
     <>
       <div className="blog-bg">
-        <Navbar />
+      <Navbar headerData={headerData}/>
         <Hero />
       </div>
       <Category />
-      <Fotter />
+      <Fotter socialMediaIcon={socialMediaIcon} tradeMarkData={tradeMarkData} footerData={footerData}/>
     </>
   );
 }
