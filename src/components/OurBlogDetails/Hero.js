efe046f11dc4ebb3d5e5877875c3126563663a6e
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-16 00:54:54 +0500

    blog image and tilte

--- src/components/OurBlogDetails/Hero.js
+++ src/components/OurBlogDetails/Hero.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function Hero() {
+export default function Hero({title=""}) {
   return (
     <>
     <div className="absolute w-full top-0  " >
       <h1 className="  text-xl md:text-4xl 2xl:text-8xl text-white f-f-b text-center pt-[136px] xl:pt-[238px] 2xl:pt-[311px] ">
-        10 Foods to Speed Up Your Metabolism
+        {/* 10 Foods to Speed Up Your Metabolism */}
+        {title}
       </h1>
       <div className="text-center mt-5 2xl:mt-12 ">
         <ul className="inline-flex">
