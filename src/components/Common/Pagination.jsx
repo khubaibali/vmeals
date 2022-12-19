44dd6fe07ffb77a8bc91ae217df34fd6c020a208
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-20 03:37:50 +0500

    Update Pagination.jsx

--- src/components/Common/Pagination.jsx
+++ src/components/Common/Pagination.jsx
@@ -10,7 +10,7 @@ export default function Pagination({ onPage, setPage, totalPages }) {
                 <nav aria-label="Page navigation example">
                     <ul className="inline-flex items-center -space-x-px">
 
-
+                    {onPage+1 !== 1 && <li><button  className={pageNumberCss} onClick={() => { setPage(0) }} >{1}</button>...</li>}
                         {[...Array(totalPages)].map((x, i) => (
                             ((onPage - i <=3 ) && ((i) < (onPage+5))) ?
                                 <li>
@@ -18,6 +18,7 @@ export default function Pagination({ onPage, setPage, totalPages }) {
                                 </li> : null
                         )
                         )}
+                        {onPage+1 !== totalPages && <li>...<button  className={pageNumberCss} onClick={() => { setPage(totalPages) }} >{totalPages}</button></li>}
                     </ul>
                 </nav>
             </div>
