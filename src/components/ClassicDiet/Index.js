a4ac8b412c781dbfef16cd48a4c9d48731ecca7b
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-20 23:17:37 +0500

    google reviews added

--- src/components/ClassicDiet/Index.js
+++ src/components/ClassicDiet/Index.js
@@ -45,7 +45,7 @@ export default function Index({ headerData, builtData, socialMediaIcon,footerDat
           <div className="bg-green-light  pt-[235px]   -mt-[241px] sm:pt-[131px] sm:-mt-[98px] lg:pt-[290px] lg:-mt-[160px] ">
             <Question faqQuestions={faqQuestions} />
           </div>
-          <Review />
+          <Review googleReviews={googleReviews} />
 
         </>
       }
