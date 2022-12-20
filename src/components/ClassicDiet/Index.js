fc4d9550c945844774753c4e92934c81c4f551a0
nabeelkhancs <34935048+nabeelkhancs@users.noreply.github.com>
2022-12-20 14:14:32 +0500

    faq on classic diet

--- src/components/ClassicDiet/Index.js
+++ src/components/ClassicDiet/Index.js
@@ -43,7 +43,7 @@ export default function Index({ headerData, builtData, socialMediaIcon, footerDa
           <Simplemenu sampleMenu={sampleMenuContent?.SampleMenu} />
           <Built builtData={builtData} />
           <div className="bg-green-light  pt-[235px]   -mt-[241px] sm:pt-[131px] sm:-mt-[98px] lg:pt-[290px] lg:-mt-[160px] ">
-            {/* <Question faqQuestions={{"0":mealPlansFaqsData?.["0"]}} /> */}
+            <Question faqQuestions={faqQuestions} />
           </div>
           <Review />
 
