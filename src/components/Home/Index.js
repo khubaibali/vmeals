cd59aeddef7bf1a19d13db4bf2a14e8117dedfd8
Shaggy <hamxa1331u@gmail.com>
2022-12-21 05:46:52 +0500

    live chat

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -8,6 +8,7 @@ const Fitness = dynamic(() => import("./Fitness"))
 const Ourblog = dynamic(() => import("./Ourblog"))
 const Fotter = dynamic(() => import("../Common/Footer"))
 import Review from '../Common/Review';
+import Script from 'next/script'
 
 
 export default function Index({ headerData, sliderBarData, builtData, ourGeniusData, homeFitnessData, ourHomeBlogData, socialMediaIcon, footerData, tradeMarkData,googleReviews }) {
@@ -26,6 +27,9 @@ export default function Index({ headerData, sliderBarData, builtData, ourGeniusD
       <Review googleReviews={googleReviews}/>
       <Ourblog ourHomeBlogData={ourHomeBlogData} />
       <Fotter socialMediaIcon={socialMediaIcon} footerData={footerData} tradeMarkData={tradeMarkData} />
+      <Script id="zsiqchat">
+        {`var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "c677e4ad620bcfc5288eb1605c582ec8668acc2aef6f63fa97c729aba3a8885efe15e56f2a17eaa7599b3f1be9b0ed22", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t)`}
+      </Script>
     </>
   );
 }
