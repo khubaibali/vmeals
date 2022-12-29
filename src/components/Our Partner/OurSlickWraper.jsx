6680f0f64a6034f7344bc542922083032454b035
Syed Khubaib Ali <76751259+khubaibali@users.noreply.github.com>
2022-12-30 01:40:33 +0500

    api replace for our partner

--- src/components/Our Partner/OurSlickWraper.jsx
+++ src/components/Our Partner/OurSlickWraper.jsx
@@ -1,7 +1,8 @@
 import Ourslick from "./ourslick";
 import {useState} from 'react'
+import RTFMapping from '../Common/RTFMapping'
 function OurSlickWraper({data,description}){
-    
+    console.log("description in rtf",description)
     return(
         <div className=" rounded-2xl  mt-5 md:mt-20  border border-green fitnessbg ">
             <div className="w-[100%]   rounded-2xl">
@@ -9,7 +10,8 @@ function OurSlickWraper({data,description}){
             </div>
             <div className="  p-5 px-8 sm:p-6 2xl:p-12  ">
               <p className="   f-f-it text-white  text-xs sm:text-tiny  2xl:text-base leading-[12px] sm:leading-[23px] text-center  ">
-               {description}{" "}
+               {/* {description}{" "} */}
+               <RTFMapping data={description}/>
               </p>
             </div>
           </div>
