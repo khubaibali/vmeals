194aa76fb134c2d85b6d3f7a21d14933fa500357
nabeelkhancs <34935048+nabeelkhancs@users.noreply.github.com>
2023-01-02 01:51:06 +0500

    lang eng

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import '../public/styles/globals.css'
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
+      <Head>
+        <html lang={"en"} />
         {/* <title>My page title</title> */}
         {/* <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" /> */}
       </Head>
-    <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
   )
-  
-  
+
+
 }
